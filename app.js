296e32b4089f58736c487cf870ac5fe7d09611f6
gilang aditya <madegilangaditya32@gmail.com>
2022-01-28 14:39:27 +0800

    finishing javascript test

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const input = [8, 7, 7, 9, 5, 4, 2, 9];
 
 function result(input) {
   // Your Code Here
+  let mid = Math.floor(input.length/2);
+  let numsInput = input.sort((a,b) => a - b);
+  let result = 0;
+  if(input.length % 2 !== 0){
+    result = numsInput[mid]; 
+  }else{
+    result =  (numsInput[mid-1] + numsInput[mid]) / 2 ;
+  }
+
+  return result;
+  
 }
 
-console.log('result(input)');
+console.log(result(input));
